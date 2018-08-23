a253aad685a8270c2f3a71cf0a1a90b1b2a944c3
Lasair <lservilla@unm.edu>
2018-08-23 15:33:05 -0600

    Initial commit

---
+++ src/AsciiArt.java
@@ -0,0 +1,23 @@
+/**
+ * @version date  2018-08-21
+ * @author Lasair Servilla
+ */
+public class AsciiArt {
+    /**
+     * It prints my first name, Lasair, in ascii art
+     *
+     */
+    public static void main(String[] args) {
+
+        System.out.println(
+            "__          ___     - _-     ___     _-_-   _-_-  - -_--\n" +
+            "| |       / /\\ \\   / /      / /\\ \\       | |     | | _ | |\n" +
+            "| |      / /__\\ \\    \\ \\   / /__\\ \\      | |    " +
+                    " | |---\\ \\\n" +
+            "|_-__-  / /    \\ \\  _ / / / /    \\ \\ _-_-   _-_- | |   " +
+                " \\ \\\n                   - -");
+
+
+    }
+}
+
